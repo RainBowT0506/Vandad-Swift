9fefc3cfba6f1a9eed65549a67b6b3bcf989c774
RainBowT <rainbowt0506@gmail.com>
2024-03-14 16:50:06 +0800

    Variables
    - Added Swift Playground code for variables, constants, and mutability
    - Demonstrated behavior of let and var with both value types and reference types
    - Illustrated mutability using arrays and NSMutableArray

--- Vandad-Swift.playground/Contents.swift
+++ Vandad-Swift.playground/Contents.swift
@@ -1,3 +1,65 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myName = "RainBowT"
+let yourName = "RT"
+
+//  Cannot use mutating member on immutable value: 'names' is a 'let' constant
+//let names = [myName,yourName]
+//names.append("Jackie")
+//names.append("Denny")
+
+
+var names = [myName,yourName]
+names.append("Jackie")
+names.append("Denny")
+
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo 2"
+foo
+foo2
+
+
+let moreNames = [
+    "志祺七七",
+    "阿滴"
+]
+
+var copy = moreNames
+copy.append("上班不要看")
+moreNames
+copy
+
+
+let oldArray = NSMutableArray(
+    array: [
+        "志祺七七",
+        "阿滴"
+    ]
+)
+
+oldArray.add("上班不要看")
+var newArray = oldArray
+newArray.add("老高玩")
+oldArray
+newArray
+
+
+let someNames = NSMutableArray(
+    array: [
+        "志祺七七",
+        "阿滴"
+    ]
+)
+
+//  Cannot assign to value: 'someNames' is a 'let' constant
+//someNames = ["館長"]
+
+func changeTheArray(_ array:NSArray){
+    let copy = array as! NSMutableArray
+    copy.add("木曜四超玩")
+}
+
+changeTheArray(someNames)
+someNames
