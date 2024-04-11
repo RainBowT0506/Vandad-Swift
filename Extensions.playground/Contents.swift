0bbc7b238bf2cc6fce7b4c0f868f6d6258cb305f
RainBowT0506 <rainbowt0506@gmail.com>
2024-04-11 15:11:57 +0800

    Extensions
    Add examples of extensions in Swift
    
    - Extending Int with a method to add two
    - Extending Person struct with a convenience initializer for full name
    - Extending Car struct to conform to GoesVroom protocol
    - Extending GoesVroom protocol with additional method
    - Adding a convenience initializer to MyDouble class

---
+++ Extensions.playground/Contents.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+extension Int{
+    func plusTwo()->Int{
+        self + 2
+    }
+}
+
+let two = 2
+two.plusTwo()
+
+struct Person{
+    let firstName:String
+    let lastName:String
+}
+
+
+extension Person{
+    init(fullName :String){
+        let components = fullName.components(separatedBy: "")
+        self.init(
+            firstName: components.first ?? fullName,
+            lastName: components.last ?? fullName)
+    }
+}
+
+let person = Person(fullName: "Foo Bar")
+person.firstName
+person.lastName
+
+
+
+
+protocol GoesVroom{
+    var vroomValue:String{get}
+    func goVroom()->String
+}
+
+extension GoesVroom{
+    func goVroom()->String{
+        "\(self.vroomValue) goes vroom!"
+    }
+}
+
+struct Car{
+    let manufactuer:String
+    let model:String
+}
+
+let modelX = Car(manufactuer: "Tesla", model: "X")
+
+extension Car:GoesVroom{
+    var vroomValue: String{
+        "\(self.manufactuer) model \(self.model)"
+    }
+}
+
+modelX.goVroom()
+
+extension GoesVroom{
+    func goVroomVroomEventMore()->String{
+        "\(self.vroomValue) is vrooming even more!"
+    }
+}
+modelX.goVroomVroomEventMore()
+
+
+class MyDouble{
+    let value:Double
+    init(value: Double) {
+        self.value = value
+    }
+}
+
+extension MyDouble{
+    convenience init(){
+        self.init(value: 0)
+    }
+}
+
+let myDouble = MyDouble()
+myDouble.value
